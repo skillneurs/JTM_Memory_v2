ebe686a292d9c6609db58924ed99b2913995d4c7
Téo <teo@example.com>
2025-05-19 14:58:43 +0200

    Corriger les chemins d'accès pour les fichiers CSS et les redirections vers home.html dans login.php et register.php

--- backend/log/login.php
+++ backend/log/login.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Connexion</title>
     <link rel="stylesheet" href="../log/log.css">
-    <link rel="stylesheet" href="../../css/font.css">
+    <link rel="stylesheet" href="../../frontend/css/font.css">
 
 </head>
 
@@ -19,7 +19,7 @@
                 <input type="password" name="mdp" placeholder="Mot de Passe" autocomplete="off">
                 <input type="submit" name="connecter" value="Se connecter" id="connecter">
                 <a href="../log/register.php">Vous navez pas de compte ? Créez en un!</a>
-                <a href="../home.html" id="btn-redirection-accueil">Continuer sans compte</a>
+                <a href="../../frontend/home.html" id="btn-redirection-accueil">Continuer sans compte</a>
             </div>
         </form>
 
@@ -42,7 +42,7 @@
                     if (password_verify($mdp, $result['mdp'])) {
                         echo "Connexion réussie !";
                         // Rediriger vers une autre page 
-                        header("Location: ../home.html");
+                        header("Location: ../../frontend/home.html");
                     } else {
                         echo "Identifiant ou mot de passe incorrect.";
                     }
