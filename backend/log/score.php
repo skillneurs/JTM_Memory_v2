f9e4ebc7d630195be23cc565edb45a6ed0004e16
Téo <teo@example.com>
2025-05-20 09:39:28 +0200

    Mise à jour de l'image de fond et redirection vers la page de score après connexion

---
+++ backend/log/score.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../log/log.css">
+    <title>Score</title>
+</head>
+<body>
+    <div class="container">
+        <h1>Score</h1>
+            <div class="champ">
+                <div id="score">Score : 0</div>
+                <div id="combo">Combo : 0</div>
+                
+                <a href="../log/login.php">Se connecter</a>
+
+            </div>
+        </form>
+    </div>
+</body>
+</html>
